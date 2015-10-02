7492ff163de56ffee7b190939a54b6569927996f
Tihomir Krystev <tihomir.krastev@mail.bg>
2015-10-02 10:22:34 +0300

    ViewPager training

---
+++ Android/ViewPager2/app/src/main/java/com/vratsasoftware/tito/viewpager2/ScreenSlidePageFragment2.java
@@ -0,0 +1,21 @@
+package com.vratsasoftware.tito.viewpager2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tito on 29.9.2015 г..
+ */
+public class ScreenSlidePageFragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_screen_slide_page2, container, false);
+
+        return rootView;
+    }
+}
---
+++ Android/ViewPager2/app/src/main/java/com/vratsasoftware/tito/viewpager2/ScreenSlidePagerActivity.java
@@ -0,0 +1,48 @@
+package com.vratsasoftware.tito.viewpager2;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+
+/**
+ * Created by Tito on 29.9.2015 г..
+ */
+public class ScreenSlidePagerActivity extends FragmentActivity {
+
+    private ViewPager mPager;
+
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_slide);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setPageTransformer(true, new DepthPageTransformer());
+
+        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+
+                invalidateOptionsMenu();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+}
+
+
+
